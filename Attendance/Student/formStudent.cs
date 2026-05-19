b0faf2b6a3153d876f898aa529fe5986ce2609a4
Chhin Chhatt <chhinchhat.ilm2050@gmail.com>
2026-05-19 23:55:33 +0700

    finish form student

---
+++ Attendance/Student/formStudent.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance.Student
+{
+    public partial class formStudent : Form
+    {
+        public formStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_student_firstname_Leave(object sender, EventArgs e)
+        {
+            ValidationInfos.InfoValidation vl = new ValidationInfos.InfoValidation();
+            vl.FirstName = txt_student_firstname.Text;
+            txt_student_firstname.Text = vl.FirstName;
+        }
+
+        private void txt_student_firstname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_student_lastname_Leave(object sender, EventArgs e)
+        {
+            ValidationInfos.InfoValidation vl = new ValidationInfos.InfoValidation();
+            vl.LastName = txt_student_lastname.Text;
+            txt_student_lastname.Text = vl.LastName;
+        }
+
+        private void txt_student_email_Leave(object sender, EventArgs e)
+        {
+            ValidationInfos.InfoValidation vl = new ValidationInfos.InfoValidation();
+            vl.Email = txt_student_email.Text;
+            txt_student_email .Text = vl.Email;
+        }
+
+        private void txt_student_phone_Leave(object sender, EventArgs e)
+        {
+            ValidationInfos.InfoValidation vl = new ValidationInfos.InfoValidation();
+            vl.PhoneNumber = txt_student_phone.Text;
+            txt_student_phone.Text = vl.PhoneNumber;
+        }
+    }
+}
---
+++ Attendance/mainForm.cs
@@ -0,0 +1,32 @@
+﻿using Attendance.Student;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Attendance
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Student.formStudent formStudent = new Student.formStudent();
+            formStudent.ShowDialog();
+        }
+    }
+}
